54a9a72a8b5cb1fc1efd9f9506b30f586139ebe8
sadiksuny <sadik.suny@uconn.edu>
2022-04-02 00:37:02 -0400

    Improved PastProjectsPage

--- src/pages/PastProjectsPage/PastProjectsPage.js
+++ src/pages/PastProjectsPage/PastProjectsPage.js
@@ -5,8 +5,9 @@ import Slider from 'react-slick';
 import pic1 from '../../assets/images/AboutPage/temp1.jpg';
 import pic2 from '../../assets/images/AboutPage/temp2.jpg';
 import pic3 from  '../../assets/images/AboutPage/temp3.jpg';
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Button from 'react-bootstrap/Button';
 
 
 function PastProjectsPage() {
@@ -17,6 +18,7 @@ function PastProjectsPage() {
         pic1,
       title: 'HackUConn 2019',
       description: 'Lorem ipsum dolor sit amet, consectur dolori',
+      link: 'https://google.com'
       
     },
     {
@@ -24,6 +26,7 @@ function PastProjectsPage() {
         pic2,
       title: 'HackUConn 2020',
       description: 'Lorem ipsum dolor sit amet, consectur dolori',
+      link:'https://facebook.com'
      
     },
     {
@@ -31,6 +34,7 @@ function PastProjectsPage() {
         pic3,
       title: 'HackUConn 2021',
       description: 'Lorem ipsum dolor sit amet, consectur dolori',
+      link:'https://twitter.com'
       
     },
   
@@ -39,18 +43,32 @@ function PastProjectsPage() {
         const [imgIndex,setImgIndex] = useState(0);
         const settings = {
            
-          centerMode:true,
-          centerPadding: "30px",
+            
             infinite: true,
             slidesToShow: 2,
-            slidesToScroll: 3,
+            slidesToScroll: 1,
             autoplay: true,
-            speed: 3500,
-            autoplaySpeed: 3500,
+            speed: 2500,
+            autoplaySpeed: 2500,
             
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 2,
+                },
+              },
+              {
+                breakpoint: 800,
+                settings: {
+                  slidesToShow: 1,
+                },
+              },
+            ],
             cssEase: "linear",
             
             beforeChange: (current, next) => setImgIndex(next), 
+            pauseOnHover: true,
             
           };
 
@@ -63,7 +81,8 @@ function PastProjectsPage() {
           <div key={index} className='card'>
             <h4 className='card-header'>{card.title}</h4>
             <img src={card.imageSrc} alt={card.title} className='card-image' width="100" height="100"/>
-            <p> {card.description}</p>
+            <p style={{padding:'1rem 1rem'}}> {card.description}</p>
+            <Button className='card-button' href={card.link}>Project Link</Button>
           </div>
         ))}
       </Slider>
