3347dc6ab57019eaac90d8ca8f658540151822e3
Jacob S Benedetti <jacob.benedetti@uconn.edu>
2022-04-05 03:54:32 -0400

    Updates

---
+++ precache-manifest.ebe81e22676995d68f2ba3d9e5904f4a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ebe4432adf81ac8a867",
+    "url": "/static/css/main.bf4de43a.chunk.css"
+  },
+  {
+    "revision": "1ebe4432adf81ac8a867",
+    "url": "/static/js/main.1ebe4432.chunk.js"
+  },
+  {
+    "revision": "cc1608aaa151681c202e",
+    "url": "/static/js/1.cc1608aa.chunk.js"
+  },
+  {
+    "revision": "d821fc18ad87df05b276",
+    "url": "/static/css/2.47283850.chunk.css"
+  },
+  {
+    "revision": "d821fc18ad87df05b276",
+    "url": "/static/js/2.d821fc18.chunk.js"
+  },
+  {
+    "revision": "25fcf04aedcb1935a0a4",
+    "url": "/static/js/runtime~main.25fcf04a.js"
+  },
+  {
+    "revision": "8301a04c76b1a556f439bb0f1288f1fc",
+    "url": "/static/media/temp1.8301a04c.jpg"
+  },
+  {
+    "revision": "d30058de824aa2dbf8e325cab72fa554",
+    "url": "/static/media/temp2.d30058de.jpg"
+  },
+  {
+    "revision": "7a5bda2ea4b0f1c88954ef82f110a045",
+    "url": "/static/media/temp3.7a5bda2e.jpg"
+  },
+  {
+    "revision": "13121f09995b9f8b5421301b3050f8da",
+    "url": "/static/media/gear.13121f09.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "242eb4009f576c230e3a1955a652cd6c",
+    "url": "/static/media/headTemp.242eb400.png"
+  },
+  {
+    "revision": "2162449975efa716887eee71a7d2294c",
+    "url": "/static/media/navbarLogo.21624499.png"
+  },
+  {
+    "revision": "6f022a929c8528b1dc0eb001570734a3",
+    "url": "/static/media/instagramLogo.6f022a92.png"
+  },
+  {
+    "revision": "94b553c874be55ebb819f09c26e21b01",
+    "url": "/static/media/background.94b553c8.png"
+  },
+  {
+    "revision": "e254a483e8261f0c7e23991b006038b0",
+    "url": "/static/media/hackuconnText.e254a483.png"
+  },
+  {
+    "revision": "bcd52ffb8d470e4f872e0b1d245a1cf4",
+    "url": "/index.html"
+  }
+];
