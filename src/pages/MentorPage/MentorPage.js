7b1a99f391f0cd2072f8d11ac0b2953c26fe9751
juliansetiadi <julian.setiadi@uconn.edu>
2022-04-06 17:48:01 -0400

    I added the format of the MentorPage, along with placeholders.

--- src/pages/MentorPage/MentorPage.js
+++ src/pages/MentorPage/MentorPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from 'react-bootstrap/Image';
-import gear from '../../assets/images/HomePage/gear.png';
+import KS from '../../assets/images/MentorPage/KS.jpg'
+import DS from '../../assets/images/MentorPage/DS.PNG'
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -12,11 +13,57 @@ class MentorPage extends React.Component {
         return(
             <div className="pt-5">
                 <Container>
-                    <Row>
+                    <Row className="mt-5">
                         <h1 className="aboutHeader">Meet The Mentors!</h1>
-                        <Col sm={4}><Image alt="gear" className="position-relative gear rotate-gears" src={gear} /></Col>
-                        <Col sm={4}><Image alt="gear" className="position-relative gear rotate-gears" src={gear} /></Col>
-                        <Col sm={4}><Image alt="gear" className="position-relative gear rotate-gears" src={gear} /></Col>
+                        <Col sm={4}>
+                            <Col><Image alt="KS" className="mentor-image" src={KS} /></Col>
+                            <Col><h1 className="name-text">Kristina Stephens</h1></Col>
+                            <Col><p className="role-text">Student Health And Wellness</p></Col>
+                        </Col>
+                        <Col sm={4}>
+                            <Col><Image alt="DS" className="mentor-image" src={DS} /></Col>
+                            <Col><h1 className="name-text">Daniel Sandberg</h1></Col>
+                            <Col><p className="role-text">Student Health And Wellness</p></Col>
+                        </Col>
+                        <Col sm={4}>
+                            <Col><Image alt="KS" className="mentor-image" src={KS} /></Col>
+                            <Col><h1 className="name-text">Kristina Stephens</h1></Col>
+                            <Col><p className="role-text">Student Health And Wellness</p></Col>
+                        </Col>
+                    </Row>
+                    <Row className="mt-5">
+                    <Col sm={4}>
+                            <Col><Image alt="KS" className="mentor-image" src={KS} /></Col>
+                            <Col><h1 className="name-text">Kristina Stephens</h1></Col>
+                            <Col><p className="role-text">Student Health And Wellness</p></Col>
+                        </Col>
+                        <Col sm={4}>
+                            <Col><Image alt="KS" className="mentor-image" src={KS} /></Col>
+                            <Col><h1 className="name-text">Kristina Stephens</h1></Col>
+                            <Col><p className="role-text">Student Health And Wellness</p></Col>
+                        </Col>
+                        <Col sm={4}>
+                            <Col><Image alt="KS" className="mentor-image" src={KS} /></Col>
+                            <Col><h1 className="name-text">Kristina Stephens</h1></Col>
+                            <Col><p className="role-text">Student Health And Wellness</p></Col>
+                        </Col>
+                    </Row>
+                    <Row className="mb-5 mt-5">
+                        <Col sm={4}>
+                            <Col><Image alt="KS" className="mentor-image" src={KS} /></Col>
+                            <Col><h1 className="name-text">Kristina Stephens</h1></Col>
+                            <Col><p className="role-text">Student Health And Wellness</p></Col>
+                        </Col>
+                        <Col sm={4}>
+                            <Col><Image alt="KS" className="mentor-image" src={KS} /></Col>
+                            <Col><h1 className="name-text">Kristina Stephens</h1></Col>
+                            <Col><p className="role-text">Student Health And Wellness</p></Col>
+                        </Col>
+                        <Col sm={4}>
+                            <Col><Image alt="KS" className="mentor-image" src={KS} /></Col>
+                            <Col><h1 className="name-text">Kristina Stephens</h1></Col>
+                            <Col><p className="role-text">Student Health And Wellness</p></Col>
+                        </Col>
                     </Row>
                 </Container>
             </div>
