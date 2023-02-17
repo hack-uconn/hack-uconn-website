c45c3af1bda66749db927d633a17db79931483ef
JulianSetiadi <Julian.setiadi@uconn.edu>
2023-02-17 11:23:48 -0500

    Started working on main video on landing page

--- src/pages/HomePage/components/DesktopHomePage.js
+++ src/pages/HomePage/components/DesktopHomePage.js
@@ -5,6 +5,7 @@ import head from '../../../assets/images/HomePage/hackuconnHead.png';
 import gear from '../../../assets/images/HomePage/gear.png';
 import NavbarPage from '../../NavbarPage/NavbarPage';
 import Container from 'react-bootstrap/Container';
+import Video from '../../../assets/images/HomePage/LandingVideo.mp4'
 import "./DesktopHomePage.css";
 
 class HomePage extends React.Component {
@@ -12,12 +13,13 @@ class HomePage extends React.Component {
         return (
             <div className='main'>
                 <NavbarPage />
+                <div className='overlay'></div>
+                <video className ='main' src = {Video} autoPlay loop muted />
                 <div className='landing'>
                     <div className="position-relative mb-3">
                         <Image alt="gear" className="position-absolute gear rotate-gears" src={gear} />
                         <Image alt="head" className="position-relative head" src={head} />
-                    </div>
-                    <div className="mt-2">
+                    </div>                        <div className="mt-2">
                         <h1>20 hour long Hackathon ● March 3rd - 4th, 2023</h1>
                         <a href="https://forms.gle/kG1QMxK4j1xSG8Nk7">
                             <Button variant="primary" className="register mt-3" size="lg">Register</Button>
--- src/pages/HomePage/components/MobileHomePage.js
+++ src/pages/HomePage/components/MobileHomePage.js
@@ -5,6 +5,7 @@ import head from '../../../assets/images/HomePage/hackuconnHead.png';
 import gear from '../../../assets/images/HomePage/gear.png';
 import NavbarPage from '../../NavbarPage/NavbarPage';
 import Container from 'react-bootstrap/Container';
+import Video from '../../../assets/images/HomePage/LandingVideo.mp4'
 import "./MobileHomePage.css";
 
 class HomePage extends React.Component {
@@ -13,12 +14,13 @@ class HomePage extends React.Component {
             <div className='main'>
                 <NavbarPage />
                 <div className='landing'>
+                    <video src = {Video} autoPlay loop muted />
                     <div className="position-relative mb-3">
                         <Image alt="gear" className="position-absolute gear rotate-gears" src={gear} />
                         <Image alt="head" className="position-relative head" src={head} />
                     </div>
                     <div className="mt-2">
-                        <h1>20 hour long Hackathon ● March 3rd - 4th, 2023</h1>
+                        <h1 className=''>20 hour long Hackathon ● March 3rd - 4th, 2023</h1>
                         <a href="https://forms.gle/kG1QMxK4j1xSG8Nk7">
                             <Button variant="primary" className="register mt-3" size="lg">Register</Button>
                         </a>
