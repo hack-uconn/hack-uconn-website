0b49f81b00df470444aba55d6da662020b710fe4
aiyer11 <iarjun2001@gmail.com>
2023-02-20 12:49:39 -0500

    Fixed redirect to exclude home page and made form open in a new tab

--- src/index.js
+++ src/index.js
@@ -1,38 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './utils/reportWebVitals';
-import AboutPage from './pages/AboutPage/AboutPage';
-import FAQPage from './pages/FAQPage/FAQPage';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import HomePage from './pages/HomePage/HomePage';
-import PastProjectsPage from './pages/PastProjectsPage/PastProjectsPage';
-import FooterPage from './pages/FooterPage/FooterPage';
-import MentorPage from './pages/MentorPage/MentorPage';
-import TestimonialsPage from './pages/TestimonialsPage/TestimonialsPage';
-import SponsorsPage from './pages/SponsorsPage/SponsorsPage';
-import AppRouter from './routes';
-import { BrowserRouter, Routes,
-  Route, } from "react-router-dom"
-import NavbarPage from './pages/NavbarPage/NavbarPage';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./utils/reportWebVitals";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import FAQPage from "./pages/FAQPage/FAQPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from "./pages/HomePage/HomePage";
+import PastProjectsPage from "./pages/PastProjectsPage/PastProjectsPage";
+import FooterPage from "./pages/FooterPage/FooterPage";
+import MentorPage from "./pages/MentorPage/MentorPage";
+import TestimonialsPage from "./pages/TestimonialsPage/TestimonialsPage";
+import SponsorsPage from "./pages/SponsorsPage/SponsorsPage";
+import AppRouter from "./routes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavbarPage from "./pages/NavbarPage/NavbarPage";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <div class ="main">
-      <NavbarPage/>
-      <HomePage />
-    </div>
-    <AppRouter/>
+      <div class="main">
+        <NavbarPage />
+      </div>
+      <AppRouter />
+      <FooterPage />
     </BrowserRouter>
-
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,26 @@
-import React from 'react'
-import AboutPage from './pages/AboutPage/AboutPage';
-import FAQPage from './pages/FAQPage/FAQPage';
-import HomePage from './pages/HomePage/HomePage';
-import PastProjectsPage from './pages/PastProjectsPage/PastProjectsPage';
-import MentorPage from './pages/MentorPage/MentorPage';
-import TestimonialsPage from './pages/TestimonialsPage/TestimonialsPage';
-import SponsorsPage from './pages/SponsorsPage/SponsorsPage';
+import React from "react";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import FAQPage from "./pages/FAQPage/FAQPage";
+import HomePage from "./pages/HomePage/HomePage";
+import PastProjectsPage from "./pages/PastProjectsPage/PastProjectsPage";
+import MentorPage from "./pages/MentorPage/MentorPage";
+import TestimonialsPage from "./pages/TestimonialsPage/TestimonialsPage";
+import SponsorsPage from "./pages/SponsorsPage/SponsorsPage";
 
-import {
-    Routes,
-    Route,
-  } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
+function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/faq" element={<FAQPage />} />
+      <Route path="/past-projects" component={PastProjectsPage} />
+      <Route path="/mentor" element={<MentorPage />} />
+      <Route path="/testimonials" component={TestimonialsPage} />
+      <Route path="/sponors" component={SponsorsPage} />
+    </Routes>
+  );
+}
 
-  function AppRouter(){
-    return (
-        <Routes>
-            <Route path="/" element={<HomePage/>}/>
-            <Route path="/about" element={<AboutPage/>}/>
-            <Route path="/faq" element={<FAQPage/>}/>
-            <Route path="/past-projects" component={PastProjectsPage}/>
-            <Route path="/mentor" element={<MentorPage/>}/>
-            <Route path="/testimonials" component={TestimonialsPage}/>
-            <Route path="/sponors" component={SponsorsPage}/>
-        </Routes>
-    );
-  }
-
-  export default AppRouter
+export default AppRouter;
