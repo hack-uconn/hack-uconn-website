7967f5bbf080d9f70ef4181df9a6226e009af171
juliansetiadi <julian.setiadi@uconn.edu>
2023-02-26 21:40:15 -0500

    Fixed spacing between sponsors and titles

--- src/pages/SponsorsPage/SponsorsPage.js
+++ src/pages/SponsorsPage/SponsorsPage.js
@@ -11,6 +11,8 @@ import CCLogo from '../../assets/images/SponsorsPage/CocaColaLogo.png'
 import global from '../../assets/images/SponsorsPage/uconnGlobal.png'
 import journalism from '../../assets/images/SponsorsPage/uconnJournalism.png'
 import philosophy from '../../assets/images/SponsorsPage/uconnPhilosophy.png'
+import inchip from '../../assets/images/SponsorsPage/uconninchip.png'
+import studentaffairs from '../../assets/images/SponsorsPage/uconnsa.png'
 import './SponsorsPage.css'
 
 class SponsorsPage extends React.Component {
@@ -18,22 +20,22 @@ class SponsorsPage extends React.Component {
         return(
             <div style={{backgroundColor:"#815AE0"}} className="pt-5">
                 <Container className="sponsors">
-                    <h1 className="headers pt-2 pb-5">HackUConn is brought to you by...</h1>
-                    <Row id="gold" className="mt-3">
+                    <h1 className="headers">HackUConn is brought to you by...</h1>
+                    <Row id="gold" className="mt-5 mb-5">
                         <h1 className="headers pt-2 pb-5">Meet our Gold Sponsors!</h1>
-                        <Col><a href="https://global.uconn.edu/"><img className="platinumGear" src={global}></img></a></Col>
+                        <Col><a href="https://global.uconn.edu/"><img className="silverGear" src={global}></img></a></Col>
                     </Row>
-                    <Row id="silver" className="mt-4">
+                    <Row id="silver" className="mt-5 mb-5">
                         <h1 className="headers pt-2 pb-5">Meet our Silver Sponsors!</h1>
-                        <Col><a href="https://philosophy.uconn.edu/"><img className="platinumGear" src={philosophy}></img></a></Col>
+                        <Col><a href="https://philosophy.uconn.edu/"><img className="goldGear" src={philosophy}></img></a></Col>
                     </Row>
-                    <Row id="bronze" className="mt-1">
+                    <Row id="gold" className="mt-5 mb-5">
                         <h1 className="headers pt-2 pb-5">Meet our Bronze Sponsors!</h1>
-                        <Col><a href="https://journalism.uconn.edu/"><img className="platinumGear" src={journalism}></img></a></Col>
+                        <Col><a href="https://journalism.uconn.edu/"><img className="goldGear" src={journalism}></img></a></Col>
+                        <Col><a href="https://chip.uconn.edu/home/"><img className="goldGear" src={inchip}></img></a></Col>
+                        <Col><a href="https://studentaffairs.uconn.edu/"><img className="goldGear" src={studentaffairs}></img></a></Col>
                     </Row>
-                    <Row id="bronze" className="mt-1 mb-1">
-                    </Row>
-                    <h1 className="headers">HackUConn is Fueled by...</h1>
+                    <h1 className="headers mt-5 mb-5">HackUConn is Fueled by...</h1>
                     <a href="https://us.coca-cola.com/"><img className="platinumGear pt-2 pb-5" src={CCLogo}></img></a>
                 </Container>
             </div>
