658491b63006e36c277907c3c21d99405c836850
juliansetiadi <julian.setiadi@uconn.edu>
2023-02-26 21:39:54 -0500

    Added Krenicki and fixed landing page

--- src/pages/HomePage/components/DesktopHomePage.js
+++ src/pages/HomePage/components/DesktopHomePage.js
@@ -9,9 +9,10 @@ import Container from 'react-bootstrap/Container';
 import AboutPage from '../../../pages/AboutPage/AboutPage';
 import FAQPage from '../../../pages/FAQPage/FAQPage';
 import FooterPage from '../../../pages/FooterPage/FooterPage';
-import opim from '../../../assets/images/HomePage/opim.png'
-import werth from '../../../assets/images/HomePage/uconnWI.png'
+import opim from '../../../assets/images/HomePage/opim.png';
+import werth from '../../../assets/images/HomePage/uconnWI.png';
 import SponsorsPage from '../../SponsorsPage/SponsorsPage';
+import krenicki from '../../../assets/images/HomePage/uconnKrenicki.png';
 
 import "./DesktopHomePage.css";
 
@@ -28,11 +29,12 @@ class HomePage extends React.Component {
                         <Image alt="gear" className="position-absolute gear rotate-gears" src={gear} />
                         <Image alt="head" className="position-relative head" src={head} />
                     </div>
-                    <div className="mb-3">
+                    <div className="sponsorLogos mb-5">
                         <a href='https://entrepreneurship.uconn.edu/'><Image alt="werth" className="logo" src={werth} /></a>
                         <a href='https://innovatelabs.uconn.edu/'><Image alt="opim" className="logo" src={opim} /></a>
+                        <a href='https://krenicki.institute.uconn.edu/'><Image alt="krenicki" className="logo" src={krenicki} /></a>
                     </div>
-                    <div className="mt-2">
+                    <div>
                         <h1>20 hour long Hackathon ● March 3rd - 4th, 2023</h1>
                         <a href="https://forms.gle/kG1QMxK4j1xSG8Nk7">
                             <Button variant="primary" className="register mt-3" size="lg">Register</Button>
