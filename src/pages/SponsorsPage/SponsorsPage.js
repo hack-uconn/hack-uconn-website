287522afd7d01442894d9f0de75b78acb439bbe6
juliansetiadi <julian.setiadi@uconn.edu>
2023-02-26 21:38:34 -0500

    Added new sponsor images

--- src/pages/SponsorsPage/SponsorsPage.js
+++ src/pages/SponsorsPage/SponsorsPage.js
@@ -36,9 +36,6 @@ class SponsorsPage extends React.Component {
                     <h1 className="headers">HackUConn is Fueled by...</h1>
                     <a href="https://us.coca-cola.com/"><img className="platinumGear pt-2 pb-5" src={CCLogo}></img></a>
                 </Container>
-                {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                    <path fill="#FFFDFA" fill-opacity="1" d="M0,128L60,144C120,160,240,192,360,186.7C480,181,600,139,720,117.3C840,96,960,96,1080,101.3C1200,107,1320,117,1380,122.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
-        </svg> */}
             </div>
         ) 
     }
